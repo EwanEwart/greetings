66adda1cfe8e4585a50ae89d8dd8087995596085
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-07-21 14:52:14 +0100

    github.com/EwanEwart/greetings: changes for v0.1.0

--- greetings.go
+++ greetings.go
@@ -1,13 +1,19 @@
 package greetings
 
+//  error : the Go standard library's errors package
 import (
+	"errors"
 	"fmt"
 	// "golang.org/x/text/message"
 )
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi %v, at Go labs.", name)
-	return message
+	return message, nil
 }
