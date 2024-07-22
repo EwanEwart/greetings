30066a8c428b593fae3935ae7780a2b44cc1bf32
EwanEwart <EwanEwart@HotMail.Co.UK>
2024-07-22 19:11:31 +0100

    greetings: greed crowd v0.2.0

--- greetings.go
+++ greetings.go
@@ -31,6 +31,76 @@ func Hello(name string) (string, error) {
 }
 
 /*
+Add a Hellos function
+whose parameter is a slice of names rather than a single name.
+Also, you change one of its return types
+from a string to a map
+so you can return names mapped to greeting messages.
+
+Hellos returns a map
+that associates each of the named people
+with a greeting message,
+and errors
+*/
+func Hellos(names []string) (map[string]string, errors) {
+	/*
+		Create a messages map
+		to associate each of the received names (as a key)
+		with a generated message (as a value).
+
+		In Go, you "initialise" a map with the following syntax:
+		make(map[key-type]value-type).
+
+		You have the Hellos function return this map to the caller.
+
+		For more about maps,
+		see Go maps in action on the Go blog:
+		https://go.dev/blog/maps
+
+		A map to associate names with messages.
+	*/
+	messages := make(map[string]string)
+	/*
+		Loop through the names your function received,
+		checking that each has a non-empty value,
+		then associate a message with each.
+
+		In this for loop,
+		range returns two values:
+		the index of the current item in the loop
+		and a copy of the item's value.
+
+		You don't need the index,
+		so you use the Go "blank identifier"
+		(an underscore) to ignore it.
+		For more, see The blank identifier in Effective Go:
+		https://go.dev/doc/effective_go.html#blank
+
+		Loop through the received slice of names,
+		calling the Hello function to get a message for each name.
+	*/
+	for _, name := range names {
+		/*
+			Have the new Hellos function
+			call the existing Hello function.
+			This helps reduce duplication
+			while also leaving both functions in place.
+		*/
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		/*
+			In the map,
+			associate the retrieved message
+			with the name.
+		*/
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+/*
 Add a randomFormat function
 that returns a randomly selected format as a greeting message.
 Note that randomFormat starts with "a lowercase letter",
